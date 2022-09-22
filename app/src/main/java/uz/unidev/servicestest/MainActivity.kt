f6b874deb5ced46f6592724278058531d2ba02a3
Nurlibay Koshkinbaev <koshkinbayevn@gmail.com>
2022-09-22 14:06:14 +0500

    Initial commit

---
+++ app/src/main/java/uz/unidev/servicestest/MainActivity.kt
@@ -0,0 +1,20 @@
+package uz.unidev.servicestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.unidev.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this, 25))
+        }
+    }
+}
